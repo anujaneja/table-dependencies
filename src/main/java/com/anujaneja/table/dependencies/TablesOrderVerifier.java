491c8e6483f7c1dde79f204f8d15f6d2750bf092
Anuj Aneja <anuj.aneja@unicommerce.com>
2020-01-28 13:20:57 +0530

    commit till now

--- src/main/java/com/anujaneja/table/dependencies/TablesOrderVerifier.java
+++ src/main/java/com/anujaneja/table/dependencies/TablesOrderVerifier.java
@@ -11,9 +11,15 @@ public class TablesOrderVerifier {
         System.out.println("tableOrders>> "+tableOrders);
         System.out.println("dependenciesList>> "+dependenciesList);
 
+        for(String tableName:tableOrders) {
+            System.out.println(tableName+"  "+dependenciesList.get(tableName));
+        }
+
         for(Map.Entry<String,ArrayList<String>> entry:dependenciesList.entrySet()){
             String key = entry.getKey();
             ArrayList<String> childList =  entry.getValue();
+
+//            System.out.println(key+"    "+childList);
 
             int keyIndex = tableOrders.indexOf(key);
 
